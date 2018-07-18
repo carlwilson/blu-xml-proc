ce3bd8f9996727d279b7e4958243e1d3c170f17d
Carl Wilson <carl@openplanetsfoundation.org>
2018-07-18 23:40:43 +0100

    FIX - tidied compiler import warnings.

--- src/main/java/org/unhcr/esafe/MimeExtensionMapper.java
+++ src/main/java/org/unhcr/esafe/MimeExtensionMapper.java
@@ -1,13 +1,9 @@
 package org.unhcr.esafe;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
